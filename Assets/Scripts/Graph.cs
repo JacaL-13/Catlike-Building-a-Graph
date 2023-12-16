36408f621e510c9f9805e1dcc1c06a1009d2ad1c
Jared Clyde <jarclyde@gmail.com>
2023-12-16 14:43:54 -0700

    Add URP assets and update graph behavior

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Graph : MonoBehaviour
@@ -10,22 +11,38 @@ public class Graph : MonoBehaviour
     [SerializeField, Range(10, 100)]
     int resolution = 10;
 
-    private void Awake()
+	Transform[] points;
+
+    void Awake()
     {
-		float step = 2f / resolution;
+        float step = 2f / resolution;
         var position = Vector3.zero;
         var scale = Vector3.one * step;
-        for (int i = 0; i < resolution; i++)
+
+		points = new Transform[resolution];
+		
+        for (int i = 0; i < points.Length; i++)
         {
-            Transform point = Instantiate(pointPrefab);
+            Transform point = points[i] = Instantiate(pointPrefab);
 
             position.x = (i + 0.5f) * step - 1f;
-            position.y = position.x * position.x;
-			
+            // position.y = position.x * position.x * position.x;
+
             point.localPosition = position;
             point.localScale = scale;
 
-			point.SetParent(transform, false);
+            point.SetParent(transform, false);
         }
     }
+
+	void Update()
+	{
+		for (int i = 0; i < points.Length; i++)
+		{
+			Transform point = points[i];
+			Vector3 position = point.localPosition;
+			position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+			point.localPosition = position;
+		}
+	}
 }
