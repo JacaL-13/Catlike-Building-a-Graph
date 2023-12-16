58b650bc09f71dc7ee33291b7ce77bd203e00187
Jared Clyde <jarclyde@gmail.com>
2023-12-16 15:11:25 -0700

    Animate sin wave

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -11,7 +11,7 @@ public class Graph : MonoBehaviour
     [SerializeField, Range(10, 100)]
     int resolution = 10;
 
-	Transform[] points;
+    Transform[] points;
 
     void Awake()
     {
@@ -19,14 +19,13 @@ public class Graph : MonoBehaviour
         var position = Vector3.zero;
         var scale = Vector3.one * step;
 
-		points = new Transform[resolution];
-		
+        points = new Transform[resolution];
+
         for (int i = 0; i < points.Length; i++)
         {
             Transform point = points[i] = Instantiate(pointPrefab);
 
             position.x = (i + 0.5f) * step - 1f;
-            // position.y = position.x * position.x * position.x;
 
             point.localPosition = position;
             point.localScale = scale;
@@ -35,14 +34,15 @@ public class Graph : MonoBehaviour
         }
     }
 
-	void Update()
-	{
-		for (int i = 0; i < points.Length; i++)
-		{
-			Transform point = points[i];
-			Vector3 position = point.localPosition;
-			position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
-			point.localPosition = position;
-		}
-	}
+    void Update()
+    {
+		float time = Time.time;
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
+        }
+    }
 }
